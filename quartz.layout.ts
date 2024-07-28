1039b5b3945ab813110f5ff694b503386b533982
Kev-gp <kevgp27@gmail.com>
2024-07-29 01:22:39 +0800

    Quartz sync: Jul 29, 2024, 1:22 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,9 +5,7 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [
-    Component.MobileOnly(Component.Graph())
-  ],
+  afterBody: [],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -41,7 +39,7 @@ export const defaultContentPageLayout: PageLayout = {
       limit: 5
     })),
     Component.MobileOnly(Component.Spacer()),
-    Component.DesktopOnly(Component.Graph()),
+    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
   ],
