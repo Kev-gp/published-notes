3a915a83ef4182697dd54aaf5d2529c0ade30fb7
Kev-gp <kevgp27@gmail.com>
2024-07-30 20:07:21 +0800

    Quartz sync: Jul 30, 2024, 8:07 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -21,7 +21,6 @@ export const defaultContentPageLayout: PageLayout = {
   ],
   left: [
     Component.PageTitle(),
-    Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
     Component.DesktopOnly(Component.RecentNotes({
