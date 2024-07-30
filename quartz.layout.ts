ec2ff804bfc6812c85232477d7aa98b8c4204ed1
Kev-gp <kevgp27@gmail.com>
2024-07-30 21:01:21 +0800

    Quartz sync: Jul 30, 2024, 9:01 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -18,8 +18,6 @@ export const defaultContentPageLayout: PageLayout = {
     Component.ArticleTitle(),
     Component.ContentMeta(),
     Component.TagList(),
-    Component.MobileOnly(Component.Spacer()),
-    Component.MobileOnly(Component.TableOfContents())
   ],
   left: [
     Component.PageTitle(),
