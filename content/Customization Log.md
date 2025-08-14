c793a32f7b5942997762688523f2923488f49fe7
Kev-gp <kevgp27@gmail.com>
2025-08-14 20:45:36 +0800

    Quartz sync: Aug 14, 2025, 8:45 PM

---
+++ content/Customization Log.md
@@ -0,0 +1,9 @@
+---
+modified_date: 2025-08-14T20:33:24+08:00
+created: 2025-08-14T17:46:44+08:00
+draft: true
+---
+## 2024
+### July
+
+- Set up 
--- quartz.layout.ts
+++ quartz.layout.ts
@@ -37,7 +37,7 @@ export const defaultContentPageLayout: PageLayout = {
           grow: true,
         },
         { Component: Component.Darkmode() },
-        { Component: Component.ReaderMode() }
+        { Component: Component.ReaderMode() },
       ]
     }),
     Component.DesktopOnly(Component.RecentNotes({
