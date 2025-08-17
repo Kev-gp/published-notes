c33372f0b7dc66e627143d438b54f5e755b547b6
Kev-gp <kevgp27@gmail.com>
2025-08-17 21:08:18 +0800

    Quartz sync: Aug 17, 2025, 9:08 PM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -13,10 +13,10 @@ export default ((opts?: Options) => {
     const links = opts?.links ?? []
     return (
       <footer class={`${displayClass ?? ""}`}>
-        <p>
+        {/* <p>
           {i18n(cfg.locale).components.footer.createdWith}{" "}
           Quartz v{version} © {year}
-        </p>
+        </p> */}
         <ul>
           {Object.entries(links).map(([text, link]) => (
             <li>
